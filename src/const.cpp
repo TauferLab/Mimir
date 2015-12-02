1bff708cd1a81de8a95a4bfcfdfa67b3f0629ae8
Tao Gao <gaotao@nudt.edu.cn>
2015-12-02 04:06:22 +0000

    Add const file.

---
+++ src/const.cpp
@@ -0,0 +1,5 @@
+#include "const.h"
+
+int oneintlen = sizeof(int);
+int twointlen = 2*sizeof(int);
+
---
+++ src/const.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANT_H
+#define CONSTANT_H
+
+extern int oneintlen;
+extern int twointlen;
+
+
+#endif
