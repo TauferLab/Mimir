97d3d321da128e1c9939ec4983290845316e389b
Tao Gao <gao381@rice-fe01.rcac.purdue.edu>
2016-05-03 11:00:56 -0400

    Add some scripts.

--- extra/clean_all.sh
+++
@@ -1,8 +0,0 @@
-start_index=1
-end_index=64
-prefix=20G
-
-for((i=$start_index;i<$end_index;i++))
-do
-  cd $prefix.$i && rm *.py *.sh *.sub* one* rr.keys* rr.points* && cd ..
-done
--- extra/run_all.sh
+++
@@ -1,8 +0,0 @@
-start_index=0
-end_index=64
-prefix=20G
-
-for((i=$start_index;i<$end_index;i++))
-do
-  mkdir $prefix.$i && cp ./script/* ./$prefix.$i && cd $prefix.$i && qsub -l nodes=1:ppn=1,walltime=04:00:00 ./gen_syn_data.sub && cd ..
-done
