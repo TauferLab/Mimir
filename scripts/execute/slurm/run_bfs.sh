034a986a513b435e5f34c723bd9039ee1f1586ac
TAO GAO <taogao@comet-ln3.sdsc.edu>
2016-08-29 13:13:37 -0700

    update some scripts

---
+++ scripts/execute/slurm/run_bfs.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./sub_jobs.sh bfs "basic partreduce partreduckvhint" "graph500" onenode "32M 64M 128M 256M 512M 1G 2G 4G 8G" "65536 131072 262144 524288 1048576 2097152 4194304 8388608 16777216" 10 none
---
+++ scripts/execute/slurm/run_octree.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./sub_jobs.sh octree "basic partreduce partreducekvhint" "1S" onenode "256M 512M 1G 2G 4G 8G 16G 32G 64G" "0.01 0.01 0.01 0.01 0.01 0.01 0.01 0.01 0.01" 10 none
