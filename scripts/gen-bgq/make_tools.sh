4177f64e243f91b7fc7ca4942224dab877bce341
Yanfei Guo <yanf.guo@gmail.com>
2016-10-13 18:05:53 -0500

    Add comment

--- scripts/gen-bgq/make_tools.sh
+++ scripts/gen-bgq/make_tools.sh
@@ -1,8 +1,8 @@
 #! /bin/zsh -e
 
-soft delete +bgqtoolchain-gcc484
-soft delete +mpiwrapper-mpich3-gcc
-soft add +mpiwrapper-bgclang-mpi3
+# soft delete +bgqtoolchain-gcc484
+# soft delete +mpiwrapper-mpich3-gcc
+# soft add +mpiwrapper-bgclang-mpi3
 
 mpiclang++11 -o gen_key_normal gen_key_normal.cc
 mpiclang++11 -o gen_wordcount_uniform gen_wordcount_uniform.cc
