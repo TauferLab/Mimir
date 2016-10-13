1f46b9cebb5ec37dca665c9ddd01ddfd0be55f36
Yanfei Guo <yanf.guo@gmail.com>
2016-10-13 18:19:16 +0000

    use bgq makefile

--- scripts/execute/bgq/make_all.sh
+++ scripts/execute/bgq/make_all.sh
@@ -6,68 +6,68 @@ echo $DIR
 FLAGS=mpicc
 
 cd ../../../src
-make clean
-make CC=$FLAGS
+make -f Makefile.bgq clean
+make -f Makefile.bgq CC=$FLAGS
 
 cd ../examples
-make clean
-make wordcount CC=$FLAGS
-make octree CC=$FLAGS
-make bfs CC=$FLAGS CFLAGS="-DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS
+make -f Makefile.bgq octree CC=$FLAGS
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_basic
 cp ./octree $DIR/octree_basic
 cp ./bfs $DIR/bfs_basic
 
-make clean
-make wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE"
-make octree CC=$FLAGS CFLAGS="-DPART_REDUCE"
-make bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE"
+make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DPART_REDUCE"
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_pr
 cp ./octree $DIR/octree_pr
 cp ./bfs $DIR/bfs_pr
 
-make clean
-make wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT"
-make octree CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT"
-make bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT -DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT"
+make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT"
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_prkvhint
 cp ./octree $DIR/octree_prkvhint
 cp ./bfs $DIR/bfs_prkvhint
 
-make clean
-make wordcount CC=$FLAGS CFLAGS="-DCOMPRESS"
-make octree CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
-make bfs CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DCOMPRESS"
+make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_cps
 cp ./octree $DIR/octree_cps
 cp ./bfs $DIR/bfs_cps
 
-make clean
-make wordcount CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
-make octree CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
-make bfs CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
+make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_cpskvhint
 cp ./octree $DIR/octree_cpskvhint
 cp ./bfs $DIR/bfs_cpskvhint
 
-make clean
-make wordcount CC=$FLAGS CFLAGS="-DKV_HINT"
-make octree CC=$FLAGS CFLAGS="-DKV_HINT"
-make bfs CC=$FLAGS CFLAGS="-DKV_HINT -DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DKV_HINT"
+make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DKV_HINT"
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DKV_HINT -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_kvhint
 cp ./octree $DIR/octree_kvhint
 cp ./bfs $DIR/bfs_kvhint
 
-make clean
-make wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT"
-make octree CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT"
-make bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
+make -f Makefile.bgq clean
+make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT"
+make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT"
+make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_cpsprkvhint
 cp ./octree $DIR/octree_cpsprkvhint
 cp ./bfs $DIR/bfs_cpsprkvhint
 
-make clean
+make -f Makefile.bgq clean
 cd ../src
-make clean
+make -f Makefile.bgq clean
 
 cd $DIR
