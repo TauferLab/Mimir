8642f24ffa93c55a46775edac61e387d15efe215
Tao Gao <taogao@russia.gcl.cis.udel.edu>
2016-11-24 20:10:10 -0500

    Cleanup analysis scripts.

--- scripts/analysis/draw_words_dist.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python draw_key_value_dist.py  words_uniform_1G.txt words_uniform_dist --ylim 0 2000
-python draw_key_value_dist.py  words_triangular_1G.txt words_triangular_dist --ylim 0 4000
-python draw_key_value_dist.py  words_wikipedia_1G.txt words_wikipeida_dist --ylim -1000 100000
-
--- scripts/analysis/syn_workplace.sh
+++
@@ -1,2 +0,0 @@
-rsync -avz --delete "capri:/usa/taogao/workspace/projects/mtmr-mpi/paper/figures/" "../figures"
-
