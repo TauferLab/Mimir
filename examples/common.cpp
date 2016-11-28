3b36881fc7abf98ed7fa5ea02aa9c7c15a185480
Tao Gao <taogao@russia.gcl.cis.udel.edu>
2016-11-28 09:33:01 -0500

    Cleanup.

--- examples/common.cpp
+++ examples/common.cpp
@@ -12,19 +12,6 @@ const char* pagesize = NULL;
 const char* ibufsize = NULL;
 int bucketsize;
 
-#if 0
-void get_time_str(char *timestr){
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    sprintf(timestr, "%d-%d-%d-%d:%d:%d", \
-        tm.tm_year + 1900, 
-        tm.tm_mon + 1, 
-        tm.tm_mday, 
-        tm.tm_hour, 
-        tm.tm_min, 
-        tm.tm_sec);
-}
-#endif
 
 void check_envars(int rank, int size){
     if(getenv("MIMIR_BUCKET_SIZE")==NULL || \
@@ -40,16 +27,12 @@ void check_envars(int rank, int size){
 void output(int rank, int size, \
     const char*prefix, const char *outdir)
 {
-    //MapReduce::output_stat("test");
     MPI_Barrier(MPI_COMM_WORLD);
 
     commsize = getenv("MIMIR_COMM_SIZE");
     pagesize = getenv("MIMIR_PAGE_SIZE");
     ibufsize = getenv("MIMIR_IBUF_SIZE");
     bucketsize = atoi(getenv("MIMIR_BUCKET_SIZE"));
-
-    //char timestr[1024];
-    //get_time_str(timestr);
 
     char filename[1024];
     sprintf(filename, "%s/%s-%d_c%s-p%s-i%s-h%d", outdir, prefix, size, \
@@ -58,96 +41,4 @@ void output(int rank, int size, \
     MapReduce::output_stat(filename);
 
     MPI_Barrier(MPI_COMM_WORLD);
-
 }
-
-#if 0
-    if(rank==0){
-        char timestr[1024];
-        get_time_str(timestr);
-
-        char filter[1024],outfile[1024];
-
-        sprintf(filter,"%s.profiler.%d.*", filename, size);
-        sprintf(outfile,"%s.profiler.%d_%s.txt",filename,size,timestr);
-
-        printf("filter=%s, outfile=%s\n", filter, outfile);
-
-#ifdef BQG
-        FILE* finalize_script = fopen(prefix, "w");
-        fprintf(finalize_script, "#!/bin/zsh\n");
-        fprintf(finalize_script, "cat %s>>%s\n", filter, outfile);
-        fprintf(finalize_script, "rm %s\n", filter);
-#else
-        char cmd[8192];
-        sprintf(cmd, "cat %s>>%s", filter, outfile);
-        int ret = system((const char*)cmd);
-        sprintf(cmd, "rm %s", filter);
-        ret = system((const char*)cmd);
-#endif
-
-        sprintf(filter,"%s.trace.%d.*", filename, size);
-        sprintf(outfile,"%s.trace.%d_%s.txt",filename,size,timestr);
-
-        printf("filter=%s, outfile=%s\n", filter, outfile);
-
-#ifdef BQG
-        fprintf(finalize_script, "cat %s>>%s\n", filter, outfile);
-        fprintf(finalize_script, "rm %s\n", filter);
-        fclose(finalize_script);
-#else
-        sprintf(cmd, "cat %s>>%s", filter, outfile);
-        ret = system((const char*)cmd);
-        sprintf(cmd, "rm %s", filter);
-        ret = system((const char*)cmd);
-#endif
-    }
-#endif
-
-
-#if 0
-void output(const char *filenarank, const char *outdir, const char *prefix, double density, MapReduce *mr1, MapReduce *mr2){
-  char header[1000];
-  char tmp[1000];
-
-  if(estimate)
-    sprintf(header, "%s/%s_d%.2f-c%s-b%s-i%s-f%d-%s.%d", \
-      outdir, prefix, density, gbufsize, blocksize, inputsize, factor, commmode, size);
-  else
-    sprintf(header, "%s/%s_d%.2f-c%s-b%s-i%s-h%d-%s.%d", \
-      outdir, prefix, density, gbufsize, blocksize, inputsize, nbucket, commmode, size);
-
-  sprintf(tmp, "%s.%d.txt", header, rank);
-
-  FILE *fp = fopen(tmp, "w+");
-  //mr1->print_stat(fp);
-  MapReduce::print_stat(mr2, fp);
-  fclose(fp);
-
-  MPI_Barrier(MPI_COMM_WORLD);
-
-  if(rank==0){
-    time_t t = time(NULL);
-    struct tm tm = *localtime(&t);
-    char tirankstr[1024];
-    sprintf(tirankstr, "%d-%d-%d-%d:%d:%d", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
-    char infile[1024+1];
-    sprintf(infile, "%s.*.txt", header);
-    char outfile[1024+1];
-    sprintf(outfile, "%s_%s.txt", header, tirankstr);
-#ifdef BGQ
-    FILE* finalize_script = fopen(prefix, "w");
-    fprintf(finalize_script, "#!/bin/zsh\n");
-    fprintf(finalize_script, "cat %s>>%s\n", infile, outfile);
-    fprintf(finalize_script, "rm %s\n", infile);
-    fclose(finalize_script);
-#else
-    char cmd[8192+1];
-    sprintf(cmd, "cat %s>>%s", infile, outfile);
-    system(cmd);
-    sprintf(cmd, "rm %s", infile);
-    system(cmd);
-#endif
-  }
-}
-#endif
--- scripts/analysis/draw_mimir_memory_and_time.sh
+++ scripts/analysis/draw_mimir_memory_and_time.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#python draw_mimir_memory_and_time.py wordcount wikipedia \
-#    singlenode 32M,64M,128M,256M,512M,1G,2G,4G,8G,16G,32G,64G \
-#    --settings "basic,kvhint,cb,cbkvhint" \
-#    --indir ../../data/comet/wc_wikipedia_singlenode_c64M-p64M-i512M-h20/ \
-#    --outfile comet-singlenode-wc-wikipedia-features-time.pdf
+python draw_mimir_memory_and_time.py wordcount wikipedia \
+    singlenode 32M,64M,128M,256M,512M,1G,2G,4G,8G,16G,32G,64G \
+    --settings "basic,kvhint,cb,cbkvhint" \
+    --indir ../../data/comet/wc_wikipedia_singlenode_c64M-p64M-i512M-h20/ \
+    --outfile comet-singlenode-wc-wikipedia-features-time.pdf
 
 python draw_mimir_memory_and_time.py bfs graph500 \
     singlenode s20,s21,s22,s23,s24,s25,s26,s27 \
@@ -12,8 +12,8 @@ python draw_mimir_memory_and_time.py bfs graph500 \
     --indir ../../data/comet/bfs_graph500_singlenode_c64M-p64M-i512M-h20/ \
     --outfile comet-singlenode-bfs-graph500-features-time.pdf
 
-#python draw_mimir_total_time.py octree 1S \
-#    singlenode p24,p25,p26,p27,p28,p29,p30,p31,p32 \
-#    --settings "basic,kvhint,cb,cbkvhint" \
-#    --indir ../../data/comet/octree_1S_singlenode_c64M-p64M-i512M-h20/ \
-#    --outfile comet-singlenode-octree-1S-features-time.pdf
+python draw_mimir_memory_and_time.py octree 1S \
+    singlenode p24,p25,p26,p27,p28,p29,p30,p31,p32 \
+    --settings "basic,kvhint,cb,cbkvhint" \
+    --indir ../../data/comet/octree_1S_singlenode_c64M-p64M-i512M-h20/ \
+    --outfile comet-singlenode-octree-1S-features-time.pdf
