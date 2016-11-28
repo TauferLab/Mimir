28917ff9d7cbb70470f2ce922be366fa20d7ef38
Yanfei Guo <yguo@cetuslac1.fst.alcf.anl.gov>
2016-11-28 16:15:03 +0000

    Update BGQ makefile and build scripts

--- scripts/execute/bgq/make_all.sh
+++ scripts/execute/bgq/make_all.sh
@@ -3,68 +3,42 @@
 DIR=$(pwd)
 echo $DIR
 
-FLAGS=mpicc
-
 cd ../../../src
 make -f Makefile.bgq clean
-make -f Makefile.bgq CC=$FLAGS
+make -f Makefile.bgq
 
 cd ../examples
 make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS
-make -f Makefile.bgq octree CC=$FLAGS
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DOUTPUT_RESULT"
+make -f Makefile.bgq wordcount
+make -f Makefile.bgq octree
+make -f Makefile.bgq bfs CFLAGS="-DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_basic
 cp ./octree $DIR/octree_basic
 cp ./bfs $DIR/bfs_basic
 
 make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE"
-make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DPART_REDUCE"
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DOUTPUT_RESULT"
-cp ./wordcount $DIR/wordcount_pr
-cp ./octree $DIR/octree_pr
-cp ./bfs $DIR/bfs_pr
+make -f Makefile.bgq wordcount CFLAGS="-DCOMBINE"
+make -f Makefile.bgq octree CFLAGS="-DCOMBINE"
+make -f Makefile.bgq bfs CFLAGS="-DCOMBINE -DOUTPUT_RESULT"
+cp ./wordcount $DIR/wordcount_cb
+cp ./octree $DIR/octree_cb
+cp ./bfs $DIR/bfs_cb
 
 make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT"
-make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT"
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DKV_HINT -DOUTPUT_RESULT"
-cp ./wordcount $DIR/wordcount_prkvhint
-cp ./octree $DIR/octree_prkvhint
-cp ./bfs $DIR/bfs_prkvhint
-
-make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DCOMPRESS"
-make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
-cp ./wordcount $DIR/wordcount_cps
-cp ./octree $DIR/octree_cps
-cp ./bfs $DIR/bfs_cps
-
-make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
-make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT"
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
-cp ./wordcount $DIR/wordcount_cpskvhint
-cp ./octree $DIR/octree_cpskvhint
-cp ./bfs $DIR/bfs_cpskvhint
-
-make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DKV_HINT"
-make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DKV_HINT"
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DKV_HINT -DOUTPUT_RESULT"
+make -f Makefile.bgq wordcount CFLAGS="-DKHINT -DVHINT"
+make -f Makefile.bgq octree CFLAGS="-DKHINT -DVHINT"
+make -f Makefile.bgq bfs CFLAGS="-DKHINT -DVHINT -DOUTPUT_RESULT"
 cp ./wordcount $DIR/wordcount_kvhint
 cp ./octree $DIR/octree_kvhint
 cp ./bfs $DIR/bfs_kvhint
 
 make -f Makefile.bgq clean
-make -f Makefile.bgq wordcount CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT"
-make -f Makefile.bgq octree CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT"
-make -f Makefile.bgq bfs CC=$FLAGS CFLAGS="-DPART_REDUCE -DCOMPRESS -DKV_HINT -DOUTPUT_RESULT"
-cp ./wordcount $DIR/wordcount_cpsprkvhint
-cp ./octree $DIR/octree_cpsprkvhint
-cp ./bfs $DIR/bfs_cpsprkvhint
+make -f Makefile.bgq wordcount CFLAGS="-DCOMBINE -DKHINT -DVHINT"
+make -f Makefile.bgq octree CFLAGS="-DCOMBINE -DKHINT -DVHINT"
+make -f Makefile.bgq bfs CFLAGS="-DCOMBINE -DKHINT -DVHINT -DOUTPUT_RESULT"
+cp ./wordcount $DIR/wordcount_cbkvhint
+cp ./octree $DIR/octree_cbkvhint
+cp ./bfs $DIR/bfs_cbkvhint
 
 make -f Makefile.bgq clean
 cd ../src
