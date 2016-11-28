1a1aa5b34d04d137f4e721fbacb73f82bf1bba3a
TAO GAO <taogao@comet-ln2.sdsc.edu>
2016-11-28 06:39:35 -0800

    Cleanup.

---
+++ scripts/execute/slurm/run_wc_singlenode.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+./sub_jobs.sh wordcount "basic cb kvhint cbkvhint" \
+    wikipedia singlenode "32M 64M 128M 256M 512M 1G 2G 4G 8G 16G 32G 64G" "" \
+    5 1 "wc_wikipedia_singlenode_c64M-p64M-i512M-h20" $1
