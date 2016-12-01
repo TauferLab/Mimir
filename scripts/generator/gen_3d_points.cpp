63771616277d4a78afaa3012a6e1d69f91d4d223
TAO GAO <taogao@comet-ln2.sdsc.edu>
2016-12-01 08:29:41 -0800

    Rewrite generator code based on Yanfei's code.

---
+++ scripts/generator/gen_3d_points.cpp
@@ -0,0 +1,103 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <random>
+
+#include "mpi.h"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int ret = 0;
+    int worldsize = 0, worldrank = 0;
+
+    double stdiv = 0.0;
+    int start_idx = 0, num_nodes = 0, num_clusters = 0;
+
+    int index = 7;
+    int num_digit = 15, range_min = -4, range_max = 4;
+
+    vector<vector<double> > cluster_centers;
+    vector<int> points_num;
+    vector<string> distr;
+
+    string key_file_prefix;
+    string outdir;
+    char path[256];
+
+    if (argc < 7) {
+        printf("Insufficient params\n");
+        return 0;
+    }
+
+    std::random_device rd;
+    std::minstd_rand gen(rd());
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &worldsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &worldrank);
+
+    key_file_prefix = string(argv[1]);
+    outdir = string(argv[2]);
+    start_idx = atoi(argv[3]);
+    num_nodes = atoi(argv[4]);
+    stdiv = atof(argv[5]);
+    num_clusters = atoi(argv[6]);
+
+    printf("The file prefix is: %s\n", key_file_prefix.c_str());
+    printf("The number of cluster is: %d\n", num_clusters);
+
+    for (int i = 0; i < num_clusters; i++) {
+        vector<double> center;
+        center.push_back(atof(argv[index  ]));
+        center.push_back(atof(argv[index+1]));
+        center.push_back(atof(argv[index+2]));
+        cluster_centers.push_back(center);
+        points_num.push_back(atoi(argv[index+3]));
+        distr.push_back(string(argv[index+4]));
+        index += 5;
+    }
+
+    // cout << "the cluster centers: " << cluster_centers << endl;
+    // cout << "the points number: " <<points_num << endl;
+    // cout << "the distribution: " << distr << endl;
+
+    for (vector<vector<double> >::iterator cluster = cluster_centers.begin();
+         cluster != cluster_centers.end(); cluster++) {
+        int num_point_incluster = points_num.front();
+        string dist = distr.front();
+
+        // Assume normal dist
+        std::normal_distribution<> dx((*cluster)[0], stdiv);
+        std::normal_distribution<> dy((*cluster)[1], stdiv);
+        std::normal_distribution<> dz((*cluster)[2], stdiv);
+
+        int point_count = 0;
+        char filename[128];
+        memset(filename, 0, 128);
+        sprintf(filename, "%s/%s.points.%d.txt", outdir.c_str(), key_file_prefix.c_str(), worldrank);
+        ofstream out_file(filename, ios::out);
+        char points_coords[128];
+        for (int point_count = 0; point_count < num_point_incluster; point_count++) {
+            memset(points_coords, 0, 128);
+            sprintf(points_coords, " %.17f %.17f %.17f\n", dx(gen), dy(gen), dz(gen));
+            out_file << points_coords;
+
+        }
+
+    }
+
+    MPI_Finalize();
+    return 0;
+}
